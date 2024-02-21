3e73826c7d57825264e5663e8f42a21188260fde
nickc01 <nickc01@outlook.com>
2024-02-21 11:49:32 -0600

    Version Bump

--- Assets/Scripts/CrystalMachinistMod.cs
+++ Assets/Scripts/CrystalMachinistMod.cs
@@ -12,7 +12,7 @@ public class CrystalMachinistMod : WeaverMod
 
     public override string GetVersion()
     {
-        return "2.0.1.0";
+        return "2.0.1.1";
     }
 
     public override void Initialize()
